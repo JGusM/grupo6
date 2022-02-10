e757f75f4df21d51ee4f312c0af75233c3faa0a3
sabrinadanielasastre@gmail.com <sabrinadanielasastre@gmail.com>
2022-02-10 20:32:38 -0300

    Creé sla vista adminDashboard, css y sus rutas

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,12 +1,15 @@
-const path = require('path');
+const path = require("path");
 
 const controlador = {
-    create:  (req, res)=>{
-        res.render('productCreate', {tituloPagina: "CARGAR PRODUCTO"})
-    },
-    edit:  (req, res)=>{
-        res.render('productEdit', {tituloPagina: "EDITAR PRODUCTO"})
-    }
-}
+  create: (req, res) => {
+    res.render("productCreate", { tituloPagina: "CARGAR PRODUCTO" });
+  },
+  edit: (req, res) => {
+    res.render("productEdit", { tituloPagina: "EDITAR PRODUCTO" });
+  },
+  dashboard: (req, res) => {
+    res.render("adminDashboard", { tituloPagina: "DASHBOARD" });
+  },
+};
 
 module.exports = controlador;
--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -1,17 +1,14 @@
 // Acá nos falta express y el router
-const express = require('express');
-const router = express.Router(); 
+const express = require("express");
+const router = express.Router();
 
 // Aća nos falta traer el controller
-const controller = require('../controllers/adminController');
-
+const controller = require("../controllers/adminController");
 
 // Acá definimos las rutas
-router.get('/cargar', controller.create); 
-router.get('/editar', controller.edit); 
-
-
-
+router.get("/cargar", controller.create);
+router.get("/editar", controller.edit);
+router.get("/dashboard", controller.dashboard);
 
 // Acá exportamos el resultado
 module.exports = router; //Exportamos todo el contenido de la ruta para hacerlo visible
