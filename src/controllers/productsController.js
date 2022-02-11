e94e237f42e4e0a7fcd4a2c3a1c0cc46ab8535f6
JGusM <jgusmei@gmail.com>
2022-02-10 23:49:29 -0300

    Agregamos JSON  a la carpeta DATA y creamos hoja products.ejs

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,8 +1,11 @@
 const path = require('path');
 
 const controlador = {
+    index: (req, res) => {
+        res.render('products', { tituloPagina: "PRODUCTOS"});
+    },
     product: (req, res)=>{
-        res.render('productDetail', {tituloPagina: "PRODUCTO"})
+        res.render('productDetail', {tituloPagina: "DETALLE PRODUCTO"})
     },
     cart: (req, res)=>{
         res.render('productCart', {tituloPagina: "CARRITO"})
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -8,6 +8,7 @@ const controller = require('../controllers/productsController');
 
 // Acá definimos las rutas
 
+router.get('/', controller.index); 
 router.get('/detail', controller.product); 
 router.get('/cart', controller.cart); 
 
@@ -18,3 +19,22 @@ router.get('/cart', controller.cart);
 
 // Acá exportamos el resultado
 module.exports = router; //Exportamos todo el contenido de la ruta para hacerlo visible
+
+/*
+
+1. /products (GET)
+Listado de productos
+2. /products/create (GET)
+Formulario de creación de productos
+3. /products/:id (GET)
+Detalle de un producto particular
+4. /products (POST)
+Acción de creación (a donde se envía el formulario)
+5. /products/:id/edit (GET)
+Formulario de edición de productos
+6. /products/:id (PUT)
+Acción de edición (a donde se envía el formulario):
+7. /products/:id (DELETE)
+Acción de borrado
+
+*/
