db34a545315b7407315f51e0dc56b859515711fa
sabrinadanielasastre@gmail.com <sabrinadanielasastre@gmail.com>
2022-05-01 20:02:12 -0300

    login  validaciones front y feedback usuario sprint 7

--- public/js/registerForm.js
+++ public/js/registerForm.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", function(){
+/*window.addEventListener("load", function(){
 
   let formulario = document.querySelector("form.login-container")
 
@@ -32,4 +32,4 @@ window.addEventListener("load", function(){
   })
 
 
-  });
+  });*/
