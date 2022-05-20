1e15cfdaa0a91b5f53aea171da0e2f9e2d4b67d9
JGusM <jgusmei@gmail.com>
2022-05-20 00:13:31 -0300

    agregue en el models Userlogin el token

--- src/database/models/Userlogin.js
+++ src/database/models/Userlogin.js
@@ -4,31 +4,34 @@ module.exports = (sequelize, dataTypes) => {
       id: {
         type: dataTypes.INTEGER,
         primaryKey: true,
-        autoIncrement: true,
+        autoIncrement: true
       },
       firstName: {
-        type: dataTypes.STRING,
+        type: dataTypes.STRING
         
       },
       lastName: {
-        type: dataTypes.STRING,
+        type: dataTypes.STRING
         
       },
       email: {
-        type: dataTypes.STRING,
+        type: dataTypes.STRING
         
       },
       password: {
         type: dataTypes.STRING
       },
       userRole: {
-        type: dataTypes.STRING,
+        type: dataTypes.STRING
         
       },
       image: {
-        type: dataTypes.STRING,
+        type: dataTypes.STRING
         
       },
+      token: {
+        type: dataTypes.STRING
+      }
     };
   
     let config = {
