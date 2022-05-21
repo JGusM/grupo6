e61423332ebe6c58314ef33c714053135bea8f1d
JGusM <jgusmei@gmail.com>
2022-05-21 11:49:41 -0300

    actualize la base de datos con mas productos

--- dh_fitness.sql
+++ dh_fitness.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-05-2022 a las 05:05:35
+-- Tiempo de generación: 21-05-2022 a las 16:47:20
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 8.1.2
 
@@ -55,7 +55,36 @@ INSERT INTO `product` (`id`, `name`, `description`, `price`, `discount`, `image`
 (11, 'HAND GRIP', 'Peso: 500gr; Dimensiones: 20 x 15 x 4 cm', 840, 5, 'hand-grip.jpeg', 1),
 (12, 'RUEDA PARA ABDOMINALES', 'Peso: 1Kg; Dimensiones: 10 x 10 x 10 cm', 2000, 5, 'rueda-abdominal.jpeg', 1),
 (13, 'BANDA LARGA', 'Peso: 500gr; Dimensiones: 10 x 10 x 2 cm', 1200, 5, 'banda-elastica.jpeg', 1),
-(14, 'MINITRAMP CON FUNDA', 'Peso: 5 kg; Dimensiones: 50 x 50 x 20 cm', 16900, 10, 'mini-tramp-funda.jpeg', 2);
+(14, 'MINITRAMP CON FUNDA', 'Peso: 5 kg; Dimensiones: 50 x 50 x 20 cm', 16900, 10, 'mini-tramp-funda.jpeg', 2),
+(21, 'SET De Bandas Elásticas', '5 Bandas Elásticas, 2 Manijas, 2 Tobilleras Regulables Y Un Encastre P/Puerta + Bolso', 3580, 20, 'set-de-5-bandas-elasticas-1.jpg', 2),
+(22, 'Colchoneta 1×0,50×0,04m', 'Colchoneta 1×0,50×0,04m Color Azul', 3200, 20, 'COLCHONETA-DE-METRO-MIR-AZUL-1-1000x1000.jpg', 2),
+(23, 'Mancuerna De Fundición Recubierta En Goma', 'Mancuerna De Fundición MIR Recubierta En Goma Varios colores', 5200, 20, 'mancuernas-mir-kit-original-1000x1000.jpg', 2),
+(24, 'Par De Mancuernas Hexagonales De Caucho', 'Par De Mancuernas Hexagonales De Caucho', 12400, 20, 'Mancuernas-Hexagonales-de-Caucho-Mir-de-15-Kg-2-1000x1000.jpg', 1),
+(25, 'Kettlebell Plástica', 'Kettlebell Plástica Varios Colores', 3600, 20, 'Kettlebells-Plasticas-1000x1000.jpg', 2),
+(26, 'Mancuerna Hexagonal De Fundición Recubierta En Goma', 'Mancuerna Hexagonal De Fundición Recubierta En Goma Varios Colores', 6100, 20, 'MANCUERNA-RECUBIERTA-EN-GOMA-HEXAGONAL-1-1000x1000.jpg', 2),
+(27, 'MANCUERNAS RECUBIERTAS EN GOMA EVERLAST', 'MANCUERNAS RECUBIERTAS EN GOMA EVERLAST Varios Colores', 3900, 20, 'MANCUERNAS-RECUBIERTAS-EN-GOMA-EVERLAST-4kg-1000x1000.jpg', 2),
+(28, 'Par De Tobilleras Reforzadas Con Ribete', 'Par De Tobilleras Reforzadas Con Ribete Varios Colores', 2700, 20, 'Tobillera-de-2-Kg-MIR-Con-Ribete-rosa-1-1000x1000.jpg', 2),
+(29, 'Barra Para Puerta Para Dominadas', 'Barra Para Puerta Para Dominadas', 5640, 20, 'BARRA-PARA-PUERTA-FÁCIL-COLOCACIÓN-1000x1000.jpg', 1),
+(30, 'Barra Push Up Eco', 'Barra Push Up Eco Varios Colores', 1960, 20, 'BARRA-PUSH-UP-ECO-1000x1000.jpg', 1),
+(31, 'Protector Cervical Para Barras Rosa', 'Protector Cervical Para Barras Rosa', 2280, 20, 'Protector-de-barra-para-cervical-1000x1000.jpg', 1),
+(32, 'Barra Olimpica Para Triceps', 'Barra Olimpica Para Triceps', 27030, 20, 'Barra-olimpica-para-triceps-1000x1000.jpg', 1),
+(33, 'Soporte/Rack De Barras Olimpicas Horizontal', 'Soporte/Rack De Barras Olimpicas Horizontal', 20500, 20, 'SoporteRack-de-barras-olimpicas-Horizontal-1000x1000.jpg', 1),
+(35, 'Cono Taza', 'Cono Taza Varios Colores', 64, 5, 'CONO-TAZA-1-1000x1000.jpg', 1),
+(36, 'Hand Grip Regulable', 'Hand Grip MIR Regulable Varios Colores', 940, 5, 'Hand-grip-regulable-4-1000x1000.jpg', 2),
+(37, 'Disco Twister', 'Disco Twister Varios Colores', 2400, 5, '2214.jpg', 2),
+(38, 'Mini Rolo De Pilates De 15cm Verde', 'Mini Rolo De Pilates De 15cm Varios Colores', 2100, 10, 'Mini-Rolo-de-pilates-de-15cm-Verde-MIR-1000x1000.jpg', 3),
+(39, 'Cielo Tierra + Tensores', 'Cielo Tierra Mir + Tensores Varios tamaños y colores', 6580, 20, 'CIELO-CIERRA-2-1-1000x1000.jpg', 2),
+(40, 'Soga De Saltar De Plástico', 'Soga De Saltar De Plástico Varios Colores', 590, 5, '2343-1.jpg', 2),
+(41, 'Soga De Saltar De Cable Con Rulemanes', 'Soga De Saltar De Cable Con Rulemanes Varios Colores', 1440, 5, '2343-1.jpg', 2),
+(42, 'Soga De Saltar De Cable ECO', 'Soga De Saltar De Cable ECO Varios Colores', 1320, 5, 'soga-de-cable-azul.jpg', 2),
+(43, 'Medicine Ball', 'Medicine Ball Varios Colores', 9850, 20, 'Medicines-Balls-MIR-con-Pique-1000x1000.jpg', 2),
+(44, 'Medicine Ball De Cuero Nacional', 'Medicine Ball De Cuero MIR Nacional Varios Colores', 6450, 20, 'Medicine-Ball-de-cuero-MIR-Nacional-1000x1000.jpg', 2),
+(45, 'Kit Completo De Valla De 1m', 'Kit Completo De Valla De 1m Varios Colores', 1840, 5, 'Kit-de-Vallas-Plasticas-Regulable-copia-1000x1000.jpg', 2),
+(46, 'Almohadilla De Balance', 'Almohadilla De Balance Varios Colores', 2700, 5, 'ALMOHADILLA-MIR-DE-BALANCE-1000x1000.jpg', 3),
+(47, 'Mini Bozu', 'Mini Bozu Varios Colores', 1160, 5, 'Mini-Bozu-1000x1000.jpg', 3),
+(48, 'Tabla De Equilibrio', 'Tabla De Equilibrio', 2440, 10, 'Tabla-de-Equilibrio-Plastica-MIR-1000x1000.jpg', 3),
+(49, 'Core Bag', 'Core Bag Varios Colores', 9450, 15, 'CORE-BAG-MIR-DE-25-KG-1000x1000.jpg', 2),
+(50, 'Chaleco De Fuerza Con Peso', 'Chaleco De Fuerza Con Peso', 11200, 15, 'CHALECO-8-KG-COD-190503-1000x1000.jpg', 2);
 
 -- --------------------------------------------------------
 
@@ -171,7 +200,7 @@ ALTER TABLE `userlogin`
 -- AUTO_INCREMENT de la tabla `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT de la tabla `productcategory`
